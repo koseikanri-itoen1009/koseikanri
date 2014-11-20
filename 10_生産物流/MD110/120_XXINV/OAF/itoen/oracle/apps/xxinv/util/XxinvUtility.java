bc269183d0289735935fe3978fc0d1caa07485ce
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 05:26:55 +0000

    {2009/03/02 18:11:06~2009/03/02 18:20:28}
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C9045

--- 10_生産物流/MD110/120_XXINV/OAF/itoen/oracle/apps/xxinv/util/XxinvUtility.java
+++ 10_生産物流/MD110/120_XXINV/OAF/itoen/oracle/apps/xxinv/util/XxinvUtility.java
@@ -1,7 +1,7 @@
 /*============================================================================
 * ファイル名 : XxinvUtility
 * 概要説明   : 移動共通関数
-* バージョン : 1.3
+* バージョン : 1.4
 *============================================================================
 * 修正履歴
 * 日付       Ver. 担当者       修正内容
@@ -10,6 +10,7 @@
 * 2008-07-10 1.1  伊藤ひとみ   isMovHdrUpdForOwnConc,isMovLineUpdForOwnConc追加
 * 2008-10-21 1.2  伊藤ひとみ   updateMovLotDetailsロック取得を変更
 * 2008-12-05 1.3  伊藤ひとみ   本番障害#452対応
+* 2008-12-06 1.4  伊藤ひとみ   本番障害#508対応
 *============================================================================
 */
 package itoen.oracle.apps.xxinv.util;
@@ -2984,6 +2985,8 @@ public class XxinvUtility
     {
       sb.append("  AND   xmril.ship_to_quantity IS NULL "  ); // 入庫実績数量      
     }
+// 2008-12-06 H.Itou Add Start
+    sb.append("  AND   NVL(xmril.delete_flg,'N') = 'N' "    ); // 削除フラグがN
     sb.append("  AND   ROWNUM = 1; "                        );
     sb.append("END; "                                       );
   
