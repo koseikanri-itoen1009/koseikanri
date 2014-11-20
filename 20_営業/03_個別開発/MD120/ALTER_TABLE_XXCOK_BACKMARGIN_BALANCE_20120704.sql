ac6996f262925404f6b4b778e3bb67e74a43bf9b
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 08:20:52 +0000

    {2012/10/05 13:09:14~2012/10/05 13:09:58}01647
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C14464

---
+++ 20_営業/03_個別開発/MD120/ALTER_TABLE_XXCOK_BACKMARGIN_BALANCE_20120704.sql
@@ -0,0 +1,7 @@
+ALTER TABLE xxcok.xxcok_backmargin_balance
+ADD (
+  PROC_TYPE    VARCHAR2(1)
+)
+/
+COMMENT ON COLUMN xxcok.xxcok_backmargin_balance.proc_type IS 'ˆ—‹æ•ª'
+/
---
+++ 20_営業/03_個別開発/MD120/ALTER_TABLE_XXCOK_REP_BM_BALANCE_20120704.sql
@@ -0,0 +1,5 @@
+ALTER TABLE xxcok.xxcok_rep_bm_balance
+MODIFY (
+  resv_payment    VARCHAR2(8)
+)
+/
