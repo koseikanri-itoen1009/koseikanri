76019aa2058607fb1b28ccf0776af7c7a84a6c6a
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 07:17:48 +0000

    {2009/09/30 9:27:00}新規追加
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C12356

---
+++ 20_営業/01_販売/MD120/ALTER_TABLE_XXCOS_REP_DIG_DV_LIST_20090929.sql
@@ -0,0 +1,6 @@
+ALTER TABLE XXCOS.XXCOS_REP_DIG_DV_LIST MODIFY
+              (
+                DLV_INVOICE_AMOUNT   NUMBER                            --î[ïiì`ï[ã‡äz
+               ,DIGEST_SALE_AMOUNT   NUMBER                            --ê›íËä|ó¶ã‡äz
+               ,BALANCE              NUMBER                            --ç∑äz
+              );
