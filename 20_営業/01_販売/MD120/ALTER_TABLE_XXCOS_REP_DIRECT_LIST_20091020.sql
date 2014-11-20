01b0533edc89c1cf13f343cd95dda7b80688ec30
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 07:22:46 +0000

    {2009/10/20 15:03:04}
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C12528

---
+++ 20_営業/01_販売/MD120/ALTER_TABLE_XXCOS_REP_DIRECT_LIST_20091020.sql
@@ -0,0 +1,9 @@
+ALTER TABLE xxcos.xxcos_rep_direct_list MODIFY
+  (
+     order_number            NUMBER       -- ó’”Ô†
+    ,order_line_no           NUMBER       -- ó’–¾×No.
+    ,line_no                 NUMBER       -- –¾×No.
+    ,order_quantity          NUMBER(13,2) -- ó’”
+    ,deliver_actual_quantity NUMBER(13,2) -- o‰×ÀÑ”
+    ,output_quantity         NUMBER(13,2) -- ·ˆÙ”
+  );
