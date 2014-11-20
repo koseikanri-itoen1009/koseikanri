65b8dd861f42bcdfa8a930646fb7a0eb73e5fa8a
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 08:27:29 +0000

    {2014/02/03 17:41:56~2014/02/03 17:43:14}01908
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C14794

--- 20_営業/05_営業/MD110/OAF/itoen/oracle/apps/xxcso/xxcso017002j/server/XxcsoQtApTaxRateVOImpl.java
+++ 20_営業/05_営業/MD110/OAF/itoen/oracle/apps/xxcso/xxcso017002j/server/XxcsoQtApTaxRateVOImpl.java
@@ -1,16 +1,20 @@
 /*============================================================================
 * ファイル名 : XxcsoQtApTaxRateVOImpl
 * 概要説明   : 仮払税率取得用ビュークラス
-* バージョン : 1.0
+* バージョン : 1.1
 *============================================================================
 * 修正履歴
 * 日付       Ver. 担当者       修正内容
 * ---------- ---- ------------ ----------------------------------------------
 * 2011-05-17 1.0  SCS桐生和幸  新規作成
+* 2013-08-22 1.1  SCSK中野徹也 【E_本稼動_10884】消費税増税対応
 *============================================================================
 */
 package itoen.oracle.apps.xxcso.xxcso017002j.server;
 import oracle.apps.fnd.framework.server.OAViewObjectImpl;
+// 2013-08-22 Ver1.1 [E_本稼動_10884] Add Start
+import oracle.jbo.domain.Date;
+// 2013-08-22 Ver1.1 [E_本稼動_10884] Add End
 /*******************************************************************************
  * 仮払税率取得のビュークラスです。
  * @author  SCS桐生和幸
@@ -26,4 +30,23 @@ public class XxcsoQtApTaxRateVOImpl extends OAViewObjectImpl
   public XxcsoQtApTaxRateVOImpl()
   {
   }
+// 2013-08-22 Ver1.1 [E_本稼動_10884] Add Start
+  /*****************************************************************************
+   * ビュー・オブジェクトの初期化を行います。
+   * @param getDate 消費税取得基準日
+   *****************************************************************************
+   */
+  public void initQuery(
+    Date getDate
+  )
+  {
+    setWhereClause(null);
+    setWhereClauseParams(null);
+
+    setWhereClauseParam(0, getDate);
+
+    executeQuery();
+
+  }
+// 2013-08-22 Ver1.1 [E_本稼動_10884] Add End
 }
