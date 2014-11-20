075874139b010b725339f65d22a61b7ad86f1590
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 07:02:59 +0000

    {2009/08/21 11:10:18}
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C11831

--- 20_営業/03_個別開発/MD100/idx/XXCOK_CUST_SHIFT_INFO_N07.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX xxcok.xxcok_cust_shift_info_n07
-ON xxcok.xxcok_cust_shift_info(
-     status
-   , base_split_flag
-   , business_vd_if_flag
-   , business_fa_if_flag
-)
-TABLESPACE xxidx2
-/
--- 20_営業/03_個別開発/MD100/idx/XXCOK_CUST_SHIFT_INFO_N08.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX xxcok.xxcok_cust_shift_info_n08
-ON xxcok.xxcok_cust_shift_info(
-     cust_shift_date
-   , status
-   , vd_inv_trnsfr_status
-   , create_chg_je_flag
-)
-TABLESPACE xxidx2
-/
