d99316475d1f4934ff98b7c803c5b4429a963c6c
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 07:22:31 +0000

    {2009/10/20 9:17:30}
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C12520

---
+++ 20_営業/03_個別開発/MD120/ALTER_TABLE_XXCOK_SELLING_TRNS_INFO_20091016.sql
@@ -0,0 +1,6 @@
+ALTER TABLE xxcok.xxcok_selling_trns_info
+ADD (
+  selling_from_cust_code    VARCHAR2(9)
+)
+/
+COMMENT ON COLUMN xxcok.xxcok_selling_trns_info.selling_from_cust_code IS 'îÑè„êUë÷å≥å⁄ãqÉRÅ[Éh'
