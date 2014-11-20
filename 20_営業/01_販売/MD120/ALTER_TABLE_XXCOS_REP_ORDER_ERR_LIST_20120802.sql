aeca8c78dacaef10f5b21740d2597c6554107eb7
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 08:22:40 +0000

    {2013/01/15 10:54:26~2013/01/15 10:54:36}01669
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C14542

--- 20_営業/01_販売/MD120/ALTER_TABLE_XXCOS_REP_ORDER_ERR_LIST_20120802.sql
+++ 20_営業/01_販売/MD120/ALTER_TABLE_XXCOS_REP_ORDER_ERR_LIST_20120802.sql
@@ -2,4 +2,4 @@ ALTER TABLE xxcos.xxcos_rep_order_err_list  ADD (
   shop_name_alt             VARCHAR2(20)
 );
 --
-COMMENT ON COLUMN XXCOS.XXCOS_EDI_ERRORS.SHOP_NAME_ALT                               IS  '店舗名称（カナ）';
+COMMENT ON COLUMN XXCOS.XXCOS_REP_ORDER_ERR_LIST.SHOP_NAME_ALT                               IS  '店舗名称（カナ）';
