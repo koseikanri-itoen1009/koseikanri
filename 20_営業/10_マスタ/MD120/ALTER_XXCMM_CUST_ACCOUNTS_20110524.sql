6243bb66c6cc1d707844180205267e3ea8ec09da
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 08:12:54 +0000

    {2011/05/26 10:03:44~2011/05/26 10:08:32}01378
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C14109

---
+++ 20_営業/10_マスタ/MD120/ALTER_XXCMM_CUST_ACCOUNTS_20110524.sql
@@ -0,0 +1,2 @@
+-- 顧客追加情報.経度コメント変更スクリプト
+COMMENT ON COLUMN apps.xxcmm_cust_accounts.longitude   IS 'ピークカット開始時刻';
---
+++ 20_営業/10_マスタ/MD120/ALTER_XXCMM_CUST_ACCOUNTS_V_20110524.sql
@@ -0,0 +1,2 @@
+-- 顧客追加情報ビュー.経度コメント変更スクリプト
+COMMENT ON COLUMN apps.xxcmm_cust_accounts_v.longitude IS 'ピークカット開始時刻';
