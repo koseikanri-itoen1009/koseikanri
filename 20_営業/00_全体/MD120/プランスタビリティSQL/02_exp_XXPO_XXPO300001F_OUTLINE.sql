13034a8374c6f7da298130193b1786a75a5597ea
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 07:46:29 +0000

    {2010/02/02 11:15:38~2010/02/02 11:16:48}00764
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C13330

---
+++ 20_営業/00_全体/MD120/プランスタビリティSQL/02_exp_XXPO_XXPO300001F_OUTLINE.sql
@@ -0,0 +1,1 @@
+exp outln/outln file=XXPO_XXPO300001F_OUTLINE.dmp tables='OL$' 'OL$HINTS' 'OL$NODES' query= \"where ol_name like \'XXPO_XXPO300001F_OUTLINE%\'\"
---
+++ 20_営業/00_全体/MD120/プランスタビリティSQL/imp_XXPO_XXPO300001F_OUTLINE.sql
@@ -0,0 +1,1 @@
+imp outln/outln file=XXPO_XXPO300001F_OUTLINE.dmp fromuser=outln ignore=Y
