c16846c65df6960e4cfea46e4e8b1647f8002299
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 08:06:08 +0000

    {2010/10/07 18:58:58~2010/10/07 19:08:16}01186
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C13857

---
+++ 20_営業/00_全体/MD120/プランスタビリティSQL/inst_XXCFO_TAX_OUTLINE_01.sql
@@ -0,0 +1,23 @@
+rem 
+------------------------------------------------------------
+-- MD.120 
+--   プランスタビリティアウトラインのインストレーションルーチン
+-- 
+-- ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+-- define orig_file      =   作成元SQLのファイル名
+-- define hint_file      =   ヒント付きSQLのファイル名
+-- define orig_ol_name   =   アウトライン名（30文字）
+-- define hint_ol_name   =   ヒント付き用の一時アウトライン名（30文字）
+-- define orig_category  =   プロファイルに設定するアウトラインカテゴリ名（30文字）
+--
+-- @@release_palnstabiliry.sql  <<<<<<<<<<<  変更不可
+-- ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
+-- 
+------------------------------------------------------------
+
+define orig_file      = XXCFO_TAX_OUTLINE_01.sql
+define hint_file      = XXCFO_TAX_OUTLINE_01_HINT.sql
+define orig_ol_name   = XXCFO_TAX_OUTLINE_01
+define hint_ol_name   = XXCFO_TAX_OUTLINE_01H
+define orig_category  = XXCFO_TAX
+@@release_palnstabiliry.sql
