265f3d1739c4814aa4b6e13a72fe55ea447131d7
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 05:23:35 +0000

    {2008/12/26 18:58:02}
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C8754

---
+++ 10_生産物流/MD110/110_XXWSH/OAF/itoen/oracle/apps/xxwsh/xxwsh920002j/server/XxwshReserveLotVOImpl.java
@@ -0,0 +1,157 @@
+/*============================================================================
+* ファイル名 : XxwshReserveLotVOImpl
+* 概要説明   : 手持数・引当可能数一覧(ロット管理品)リージョンビューオブジェクト
+* バージョン : 1.0
+*============================================================================
+* 修正履歴
+* 日付       Ver. 担当者       修正内容
+* ---------- ---- ------------ ----------------------------------------------
+* 2008-12-25 1.0  二瓶　大輔     新規作成 本番#771対応
+*============================================================================
+*/
+package itoen.oracle.apps.xxwsh.xxwsh920002j.server;
+import oracle.apps.fnd.framework.server.OAViewObjectImpl;
+import oracle.jbo.domain.Date;
+import oracle.jbo.domain.Number;
+import itoen.oracle.apps.xxcmn.util.XxcmnUtility;
+import itoen.oracle.apps.xxwsh.util.XxwshConstants;
+import com.sun.java.util.collections.HashMap;
+
+/***************************************************************************
+ * 手持数・引当可能数一覧(ロット管理品)リージョンビューオブジェクトクラスです。
+ * @author  ORACLE 二瓶　大輔
+ * @version 1.0
+ ***************************************************************************
+ */
+public class XxwshReserveLotVOImpl extends OAViewObjectImpl
+{
+  /**
+   * 
+   * This is the default constructor (do not remove)
+   */
+  public XxwshReserveLotVOImpl()
+  {
+  }
+  /*****************************************************************************
+   * VOの初期化を行います。
+   * @param params - 検索キー
+   ****************************************************************************/
+  public void initQuery(
+    HashMap params)
+  {
+    // 検索キーが指定されている場合に検索を実行
+    if (!XxcmnUtility.isBlankOrNull(params))
+    {
+
+      // HashMapから値を取得
+      Number itemId                     = (Number)params.get("ItemId");                     // 品目ID
+      Number inputInventoryLocationId   = (Number)params.get("InputInventoryLocationId");   // 保管倉庫ID
+      String inputInventoryLocationCode = (String)params.get("InputInventoryLocationCode"); // 保管倉庫コード
+      String documentTypeCode           = (String)params.get("DocumentTypeCode");           // 文書タイプ
+      String locationRelCode            = (String)params.get("LocationRelCode");            // 拠点実績有無区分
+      String convUnitUseKbn             = (String)params.get("ConvUnitUseKbn");             // 入出庫換算単位使用区分
+      String callPictureKbn             = (String)params.get("CallPictureKbn");             // 呼出画面区分
+      Number lotCtl                     = (Number)params.get("LotCtl");                     // ロット管理品
+      String designatedProductionDate   = (String)params.get("DesignatedProductionDate");   // 指定製造日
+      Number lineId                     = (Number)params.get("LineId");                     // 明細ID
+      Date scheduleShipDate             = (Date)params.get("ScheduleShipDate");             // 出荷予定日
+      String prodClass                  = (String)params.get("ProdClass");                  // 商品区分
+      String itemClass                  = (String)params.get("ItemClass");                  // 品目区分
+      String numOfCases                 = (String)params.get("NumOfCases");                 // ケース入数
+      String frequentWhseCode           = (String)params.get("FrequentWhseCode");            // 代表倉庫
+      String masterOrgId                = (String)params.get("MasterOrgId");                 // 在庫組織ID
+      String maxDate                    = (String)params.get("MaxDate");                     // 最大日付
+      String dummyFrequentWhse          = (String)params.get("DummyFrequentWhse");           // ダミー倉庫
+ 
+      // WHERE句を初期化
+      setWhereClauseParams(null); // Always reset
+      setOrderByClause(null);
+      setWhereClause(null);
+      // バインド変数に値をセット
+      int i = 0;
+      setWhereClauseParam(i++, convUnitUseKbn);             //  0:入出庫換算単位使用区分
+      setWhereClauseParam(i++, numOfCases);                 //  1:ケース入数
+      setWhereClauseParam(i++, convUnitUseKbn);             //  2:入出庫換算単位使用区分
+      setWhereClauseParam(i++, numOfCases);                 //  3:ケース入数
+      setWhereClauseParam(i++, lotCtl);                     //  4:ロット管理
+      setWhereClauseParam(i++, lotCtl);                     //  5:ロット管理
+      setWhereClauseParam(i++, masterOrgId);                //  6:在庫組織ID
+      setWhereClauseParam(i++, scheduleShipDate);           //  7:出庫予定日
+      setWhereClauseParam(i++, maxDate);                    //  8:最大日付
+      setWhereClauseParam(i++, inputInventoryLocationId);   //  9:入力保管倉庫ID
+      setWhereClauseParam(i++, inputInventoryLocationCode); // 10:入力保管倉庫コード
+      setWhereClauseParam(i++, frequentWhseCode);           // 11:代表倉庫
+      setWhereClauseParam(i++, dummyFrequentWhse);          // 12:ダミー倉庫
+      setWhereClauseParam(i++, lotCtl);                     // 13:ロット管理
+      setWhereClauseParam(i++, inputInventoryLocationId);   // 14:入力保管倉庫ID
+      setWhereClauseParam(i++, convUnitUseKbn);             // 15:入出庫換算単位使用区分
+      setWhereClauseParam(i++, numOfCases);                 // 16:ケース入数
+      setWhereClauseParam(i++, itemId);                     // 17:品目ID
+      setWhereClauseParam(i++, prodClass);                  // 18:商品区分
+      setWhereClauseParam(i++, lineId);                     // 19:明細ID
+      setWhereClauseParam(i++, documentTypeCode);           // 20:文書タイプ
+      setWhereClauseParam(i++, scheduleShipDate);           // 21:出庫予定日
+      setWhereClauseParam(i++, scheduleShipDate);           // 22:出庫予定日
+
+      // ロット管理品の場合条件をセット
+      if (XxwshConstants.LOT_CTL_Y.equals(lotCtl.toString()))
+      {
+        //WHERE句作成
+        StringBuffer whereClause   = new StringBuffer(1000);  // WHERE句作成用オブジェクト
+        //ORDERBY句生成
+        StringBuffer orderByClause = new StringBuffer(1000);  // ORDERBY句作成用オブジェクト
+        //条件にロット管理品を追加
+        whereClause.append(" lot_id <> " + XxwshConstants.DEFAULT_LOT.toString());
+
+        //指定製造日が入力されている場合条件を追加
+        if (!XxcmnUtility.isBlankOrNull(designatedProductionDate))
+        {
+          XxcmnUtility.andAppend(whereClause);
+          // Where句生成
+          whereClause.append(" production_date >= '" + designatedProductionDate + "'");
+        }
+        //呼出画面区分が「出荷」で拠点実績有無区分が「売上拠点」の場合条件を追加
+        if (XxwshConstants.CALL_PIC_KBN_SHIP_INPUT.equals(callPictureKbn) &
+           XxwshConstants.LOCATION_REL_CODE_SALE.equals(locationRelCode))
+        {
+          XxcmnUtility.andAppend(whereClause);
+          // Where句生成
+          whereClause.append(" ship_req_m_reserve = 'Y'");
+        }
+        // 呼出画面区分が「支給」の場合条件を追加
+        else if(XxwshConstants.CALL_PIC_KBN_PROD_CREATE.equals(callPictureKbn))
+        {
+          XxcmnUtility.andAppend(whereClause);
+          // Where句生成
+          whereClause.append(" pay_provision_m_reserve = 'Y'");
+        }
+        // 呼出画面区分が「移動」の場合条件を追加
+        else if(XxwshConstants.CALL_PIC_KBN_MOVE_ORDER.equals(callPictureKbn))
+        {
+          XxcmnUtility.andAppend(whereClause);
+          // Where句生成
+          whereClause.append(" move_inst_m_reserve = 'Y'");
+        }
+        // ORDER BY句を設定
+        // 品目区分が製品の場合
+        if (XxwshConstants.ITEM_TYPE_PROD.equals(itemClass))
+        {
+          // Order BY句生成
+          orderByClause.append(" production_date asc "); // 製造日(昇順)
+          orderByClause.append(",expiration_date asc "); // 賞味期限(昇順)
+          orderByClause.append(",uniqe_sign asc ");      // 固有記号(昇順)
+        } else
+        {
+          // Order BY句生成
+          orderByClause.append(" TO_NUMBER(show_lot_no) asc ");     // ロットNo(昇順)
+        }
+        //追加検索条件をセット
+        setWhereClause(whereClause.toString());
+        // ORDER BY 条件をセット
+        setOrderByClause(orderByClause.toString());
+      }
+      //検索を実行
+      executeQuery();
+    }
+  }
+}
---
+++ 10_生産物流/MD110/110_XXWSH/OAF/itoen/oracle/apps/xxwsh/xxwsh920002j/server/XxwshReserveUnLotVOImpl.java
@@ -0,0 +1,146 @@
+/*============================================================================
+* ファイル名 : XxwshStockCanEncQtyVOImpl
+* 概要説明   : 手持数・引当可能数一覧(ロット管理外品)リージョンビューオブジェクト
+* バージョン : 1.0
+*============================================================================
+* 修正履歴
+* 日付       Ver. 担当者       修正内容
+* ---------- ---- ------------ ----------------------------------------------
+* 2008-12-25 1.0  二瓶　大輔     新規作成 本番#771対応
+*============================================================================
+*/
+package itoen.oracle.apps.xxwsh.xxwsh920002j.server;
+import oracle.apps.fnd.framework.server.OAViewObjectImpl;
+import oracle.jbo.domain.Date;
+import oracle.jbo.domain.Number;
+import itoen.oracle.apps.xxcmn.util.XxcmnUtility;
+import itoen.oracle.apps.xxwsh.util.XxwshConstants;
+import com.sun.java.util.collections.HashMap;
+
+/***************************************************************************
+ * 手持数・引当可能数一覧(ロット管理外品)リージョンビューオブジェクトクラスです。
+ * @author  ORACLE 二瓶　大輔
+ * @version 1.0
+ ***************************************************************************
+ */
+public class XxwshReserveUnLotVOImpl extends OAViewObjectImpl
+{
+  /**
+   * 
+   * This is the default constructor (do not remove)
+   */
+  public XxwshReserveUnLotVOImpl()
+  {
+  }
+  /*****************************************************************************
+   * VOの初期化を行います。
+   * @param params - 検索キー
+   ****************************************************************************/
+  public void initQuery(
+    HashMap params)
+  {
+    // 検索キーが指定されている場合に検索を実行
+    if (!XxcmnUtility.isBlankOrNull(params))
+    {
+
+      // HashMapから値を取得
+      Number itemId                     = (Number)params.get("ItemId");                     // 品目ID
+      Number inputInventoryLocationId   = (Number)params.get("InputInventoryLocationId");   // 保管倉庫ID
+      String documentTypeCode           = (String)params.get("DocumentTypeCode");           // 文書タイプ
+      String locationRelCode            = (String)params.get("LocationRelCode");            // 拠点実績有無区分
+      String convUnitUseKbn             = (String)params.get("ConvUnitUseKbn");             // 入出庫換算単位使用区分
+      String callPictureKbn             = (String)params.get("CallPictureKbn");             // 呼出画面区分
+      Number lotCtl                     = (Number)params.get("LotCtl");                     // ロット管理品
+      String designatedProductionDate   = (String)params.get("DesignatedProductionDate");   // 指定製造日
+      Number lineId                     = (Number)params.get("LineId");                     // 明細ID
+      Date scheduleShipDate             = (Date)params.get("ScheduleShipDate");             // 出荷予定日
+      String prodClass                  = (String)params.get("ProdClass");                  // 商品区分
+      String itemClass                  = (String)params.get("ItemClass");                  // 品目区分
+      String numOfCases                 = (String)params.get("NumOfCases");                 // ケース入数
+ 
+      // WHERE句を初期化
+      setWhereClauseParams(null); // Always reset
+      setOrderByClause(null);
+      setWhereClause(null);
+      // バインド変数に値をセット
+      int i = 0;
+      setWhereClauseParam(i++, convUnitUseKbn);           // 入出庫換算単位使用区分
+      setWhereClauseParam(i++, numOfCases);               // ケース入数
+      setWhereClauseParam(i++, convUnitUseKbn);           // 入出庫換算単位使用区分
+      setWhereClauseParam(i++, numOfCases);               // ケース入数
+      setWhereClauseParam(i++, lotCtl);                   // ロット管理
+      setWhereClauseParam(i++, inputInventoryLocationId); // 入力保管倉庫ID
+      setWhereClauseParam(i++, lotCtl);                   // ロット管理
+      setWhereClauseParam(i++, scheduleShipDate);         // 出庫予定日
+      setWhereClauseParam(i++, inputInventoryLocationId); // 入力保管倉庫ID
+      setWhereClauseParam(i++, lotCtl);                   // ロット管理
+      setWhereClauseParam(i++, convUnitUseKbn);           // 入出庫換算単位使用区分
+      setWhereClauseParam(i++, numOfCases);               // ケース入数
+      setWhereClauseParam(i++, itemId);                   // 品目ID
+      setWhereClauseParam(i++, prodClass);                // 商品区分
+      setWhereClauseParam(i++, lineId);                   // 明細ID
+      setWhereClauseParam(i++, documentTypeCode);         // 文書タイプ
+      setWhereClauseParam(i++, scheduleShipDate);         // 出庫予定日
+      setWhereClauseParam(i++, scheduleShipDate);         // 出庫予定日
+      // ロット管理品の場合条件をセット
+      if (XxwshConstants.LOT_CTL_Y.equals(lotCtl.toString()))
+      {
+        //WHERE句作成
+        StringBuffer whereClause   = new StringBuffer(1000);  // WHERE句作成用オブジェクト
+        //ORDERBY句生成
+        StringBuffer orderByClause = new StringBuffer(1000);  // ORDERBY句作成用オブジェクト
+        //条件にロット管理品を追加
+        whereClause.append(" lot_id <> " + XxwshConstants.DEFAULT_LOT.toString());
+
+        //指定製造日が入力されている場合条件を追加
+        if (!XxcmnUtility.isBlankOrNull(designatedProductionDate))
+        {
+          XxcmnUtility.andAppend(whereClause);
+          // Where句生成
+          whereClause.append(" production_date >= '" + designatedProductionDate + "'");
+        }
+        //呼出画面区分が「出荷」で拠点実績有無区分が「売上拠点」の場合条件を追加
+        if (XxwshConstants.CALL_PIC_KBN_SHIP_INPUT.equals(callPictureKbn) &
+           XxwshConstants.LOCATION_REL_CODE_SALE.equals(locationRelCode))
+        {
+          XxcmnUtility.andAppend(whereClause);
+          // Where句生成
+          whereClause.append(" ship_req_m_reserve = 'Y'");
+        }
+        // 呼出画面区分が「支給」の場合条件を追加
+        else if(XxwshConstants.CALL_PIC_KBN_PROD_CREATE.equals(callPictureKbn))
+        {
+          XxcmnUtility.andAppend(whereClause);
+          // Where句生成
+          whereClause.append(" pay_provision_m_reserve = 'Y'");
+        }
+        // 呼出画面区分が「移動」の場合条件を追加
+        else if(XxwshConstants.CALL_PIC_KBN_MOVE_ORDER.equals(callPictureKbn))
+        {
+          XxcmnUtility.andAppend(whereClause);
+          // Where句生成
+          whereClause.append(" move_inst_m_reserve = 'Y'");
+        }
+        // ORDER BY句を設定
+        // 品目区分が製品の場合
+        if (XxwshConstants.ITEM_TYPE_PROD.equals(itemClass))
+        {
+          // Order BY句生成
+          orderByClause.append(" production_date asc "); // 製造日(昇順)
+          orderByClause.append(",expiration_date asc "); // 賞味期限(昇順)
+          orderByClause.append(",uniqe_sign asc ");      // 固有記号(昇順)
+        } else
+        {
+          // Order BY句生成
+          orderByClause.append(" TO_NUMBER(show_lot_no) asc ");     // ロットNo(昇順)
+        }
+        //追加検索条件をセット
+        setWhereClause(whereClause.toString());
+        // ORDER BY 条件をセット
+        setOrderByClause(orderByClause.toString());
+      }
+      //検索を実行
+      executeQuery();
+    }
+  }
+}
