3609d803c517e07d30502196ff1b7d24e1fbd50a
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 08:12:20 +0000

    {2011/05/12 18:44:46}y
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C14089

---
+++ 20_営業/01_販売/MD120/ALTER_TABLE_XXCOS_REP_DIG_DV_LIST_07098.sql
@@ -0,0 +1,6 @@
+ALTER TABLE xxcos.xxcos_rep_dig_dv_list ADD (
+                    digestion_due_date  DATE    -- Á‰»ŒvZ’÷“ú
+)
+;
+COMMENT ON COLUMN xxcos.xxcos_rep_dig_dv_list.digestion_due_date IS 'Á‰»ŒvZ’÷“ú'
+;
