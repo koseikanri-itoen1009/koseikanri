fa7e98add7ec781d793f2e983fd399724d1d05ba
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 04:59:41 +0000

    {2008/08/01 9:57:32~2008/08/01 10:05:14}
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C7887

---
+++ 10_生産物流/MD120/script/DB/xxwip_create_pkg.sql
@@ -0,0 +1,72 @@
+PROMPT xxwip_common2_pkg.pkb >>>
+@xxwip_common2_pkg.pkb;
+PROMPT xxwip_common2_pkg.pks >>>
+@xxwip_common2_pkg.pks;
+PROMPT xxwip_common3_pkg.pkb >>>
+@xxwip_common3_pkg.pkb;
+PROMPT xxwip_common3_pkg.pks >>>
+@xxwip_common3_pkg.pks;
+PROMPT xxwip_common_pkg.pkb >>>
+@xxwip_common_pkg.pkb;
+PROMPT xxwip_common_pkg.pks >>>
+@xxwip_common_pkg.pks;
+PROMPT XXWIP200001C.pkb >>>
+@XXWIP200001C.pkb;
+PROMPT XXWIP200001C.pks >>>
+@XXWIP200001C.pks;
+PROMPT XXWIP200003C.pkb >>>
+@XXWIP200003C.pkb;
+PROMPT XXWIP200003C.pks >>>
+@XXWIP200003C.pks;
+PROMPT XXWIP210001C.pkb >>>
+@XXWIP210001C.pkb;
+PROMPT XXWIP210001C.pks >>>
+@XXWIP210001C.pks;
+PROMPT XXWIP230001C.pkb >>>
+@XXWIP230001C.pkb;
+PROMPT XXWIP230001C.pks >>>
+@XXWIP230001C.pks;
+PROMPT XXWIP230002C.pkb >>>
+@XXWIP230002C.pkb;
+PROMPT XXWIP230002C.pks >>>
+@XXWIP230002C.pks;
+PROMPT XXWIP720001C.pkb >>>
+@XXWIP720001C.pkb;
+PROMPT XXWIP720001C.pks >>>
+@XXWIP720001C.pks;
+PROMPT XXWIP720002C.pkb >>>
+@XXWIP720002C.pkb;
+PROMPT XXWIP720002C.pks >>>
+@XXWIP720002C.pks;
+PROMPT xxwip730001c.pkb >>>
+@xxwip730001c.pkb;
+PROMPT xxwip730001c.pks >>>
+@xxwip730001c.pks;
+PROMPT xxwip730002c.pkb >>>
+@xxwip730002c.pkb;
+PROMPT xxwip730002c.pks >>>
+@xxwip730002c.pks;
+PROMPT xxwip730003c.pkb >>>
+@xxwip730003c.pkb;
+PROMPT xxwip730003c.pks >>>
+@xxwip730003c.pks;
+PROMPT XXWIP730004C.pkb >>>
+@XXWIP730004C.pkb;
+PROMPT XXWIP730004C.pks >>>
+@XXWIP730004C.pks;
+PROMPT XXWIP730005C.pkb >>>
+@XXWIP730005C.pkb;
+PROMPT XXWIP730005C.pks >>>
+@XXWIP730005C.pks;
+PROMPT xxwip740001c.pkb >>>
+@xxwip740001c.pkb;
+PROMPT xxwip740001c.pks >>>
+@xxwip740001c.pks;
+PROMPT XXWIP740002C.pkb >>>
+@XXWIP740002C.pkb;
+PROMPT XXWIP740002C.pks >>>
+@XXWIP740002C.pks;
+PROMPT XXWIP750001C.pkb >>>
+@XXWIP750001C.pkb;
+PROMPT XXWIP750001C.pks >>>
+@XXWIP750001C.pks;
