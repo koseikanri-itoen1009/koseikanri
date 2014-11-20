0447082ba3f6a14c04fb228c3973f7e154529816
SourceSafeUser Oracle <SourceSafeUser Oracle>
2014-11-20 07:44:24 +0000

    {2010/01/21 11:50:44~2010/01/21 11:51:06}00690
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C13271

--- 10_生産物流/MD110/130_XXWIP/OAF/itoen/oracle/apps/xxwip/lov/webui/ItemNoLovCO.java
+++ 10_生産物流/MD110/130_XXWIP/OAF/itoen/oracle/apps/xxwip/lov/webui/ItemNoLovCO.java
@@ -1,12 +1,13 @@
 /*============================================================================
 * ファイル名 : ItemNoLovCO
 * 概要説明   : 品目値リストコントローラ
-* バージョン : 1.0
+* バージョン : 1.1
 *============================================================================
 * 修正履歴
 * 日付       Ver. 担当者       修正内容
 * ---------- ---- ------------ ----------------------------------------------
 * 2007-12-28 1.0  二瓶大輔     新規作成
+* 2010-01-18 1.1  伊藤ひとみ   本稼動障害#1151
 *============================================================================
 */
 package itoen.oracle.apps.xxwip.lov.webui;
@@ -23,7 +24,7 @@ import oracle.apps.fnd.framework.OAApplicationModule;
 /***************************************************************************
  * 品目値リストコントローラクラスです。
  * @author  ORACLE 二瓶 大輔
- * @version 1.0
+ * @version 1.1
  ***************************************************************************
  */
 public class ItemNoLovCO extends OAControllerImpl
@@ -58,6 +59,14 @@ public class ItemNoLovCO extends OAControllerImpl
     vo.setWhereClause (null); // clean up from previous invokation
     vo.setWhereClauseParams (null); // clean up from previous invokation.
     vo.setWhereClauseParam(0, invLocId);
+// 2010-01-18 H.Itou Add Start 本稼動障害#1151
+    vo.setWhereClauseParam(1, invLocId);
+    vo.setWhereClauseParam(2, invLocId);
+    vo.setWhereClauseParam(3, invLocId);
+    vo.setWhereClauseParam(4, invLocId);
+    vo.setWhereClauseParam(5, invLocId);
+    vo.setWhereClauseParam(6, invLocId);
+// 2010-01-18 H.Itou Add End
     if ("1".equals(destinationType)) 
     {
       StringBuffer whereClause = new StringBuffer(1000);  // WHERE句作成用オブジェクト
