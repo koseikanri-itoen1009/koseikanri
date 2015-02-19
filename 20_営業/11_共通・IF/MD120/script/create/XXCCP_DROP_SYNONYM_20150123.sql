7f207930d2b93c9245125864590d4da17767df99
VisualStudio2013 <ACOMSV01\VisualStudio>
2015-02-19 01:37:29 +0000

    02128
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C15498

---
+++ 20_営業/11_共通・IF/MD120/script/create/XXCCP_DROP_SYNONYM_20150123.sql
@@ -0,0 +1,40 @@
+/****************************************************************************************
+ * Copyright(c)Sumisho Computer Systems Corporation, 2008. All rights reserved.
+ *
+ * Script Name     : XXCCP_DROP_SYNONYM_20150123
+ * Description     : 参照ユーザー用シノニムスクリプト
+ * Version         : 1.1
+ *
+ * Change Record
+ * ------------- ----- ---------------- ---------------------------------
+ *  Date          Ver.  Editor           Description
+ * ------------- ----- ---------------- ---------------------------------
+ *  2015/01/23    1.0   T.Ishiwata       新規作成
+ *
+ ****************************************************************************************/
+DROP SYNONYM scs01.fnd_languages
+/
+DROP SYNONYM scs02.fnd_languages
+/
+DROP SYNONYM scs03.fnd_languages
+/
+DROP SYNONYM scs04.fnd_languages
+/
+DROP SYNONYM scs05.fnd_languages
+/
+DROP SYNONYM scs06.fnd_languages
+/
+DROP SYNONYM scs07.fnd_languages
+/
+DROP SYNONYM scs08.fnd_languages
+/
+DROP SYNONYM scs09.fnd_languages
+/
+DROP SYNONYM scs10.fnd_languages
+/
+DROP SYNONYM itnadmin01.fnd_languages
+/
+DROP SYNONYM itnadmin02.fnd_languages
+/
+
+
