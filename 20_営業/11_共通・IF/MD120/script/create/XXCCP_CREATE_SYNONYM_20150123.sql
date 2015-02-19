f553a326f1ebd47bf197e73e8137787d1f6bb7f0
VisualStudio2013 <ACOMSV01\VisualStudio>
2015-02-19 01:38:28 +0000

    02128
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C15499

---
+++ 20_営業/11_共通・IF/MD120/script/create/XXCCP_CREATE_SYNONYM_20150123.sql
@@ -0,0 +1,40 @@
+/****************************************************************************************
+ * Copyright(c)Sumisho Computer Systems Corporation, 2008. All rights reserved.
+ *
+ * Script Name     : XXCCP_CREATE_SYNONYM_20150123
+ * Description     : 参照ユーザー用シノニムスクリプト
+ * Version         : 1.1
+ *
+ * Change Record
+ * ------------- ----- ---------------- ---------------------------------
+ *  Date          Ver.  Editor           Description
+ * ------------- ----- ---------------- ---------------------------------
+ *  2015/01/23    1.0   T.Ishiwata       新規作成
+ *
+ ****************************************************************************************/
+CREATE SYNONYM scs01.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM scs02.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM scs03.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM scs04.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM scs05.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM scs06.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM scs07.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM scs08.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM scs09.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM scs10.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM itnadmin01.fnd_languages FOR APPS.fnd_languages
+/
+CREATE SYNONYM itnadmin02.fnd_languages FOR APPS.fnd_languages
+/
+
+
