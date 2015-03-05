516eb75827593fdee9ea709fc2ebf7d84406c31e
Administrator <ITOEN.MASTER\Administrator>
2015-03-05 10:55:25 +0000

    02117
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C15711

---
+++ 20_営業/05_営業/MD110/OAF/itoen/oracle/apps/xxcso/xxcso010003j/server/XxcsoSpDecisionHeadersSummuryVOImpl.java
@@ -0,0 +1,31 @@
+/*============================================================================
+* ファイル名 : XxcsoSpDecisionHeadersSummuryVOImpl
+* 概要説明   : SP専決ヘッダサマリ情報取得ビューオブジェクトクラス
+* バージョン : 1.0
+*============================================================================
+* 修正履歴
+* 日付       Ver. 担当者       修正内容
+* ---------- ---- ------------ ----------------------------------------------
+* 2015-02-02 1.0  SCSK山下翔太 新規作成
+*============================================================================
+*/
+package itoen.oracle.apps.xxcso.xxcso010003j.server;
+
+import oracle.apps.fnd.framework.server.OAViewObjectImpl;
+
+/*******************************************************************************
+ * SP専決ヘッダサマリ情報取得ビューオブジェクトクラス
+ * @author  SCSK山下翔太
+ * @version 1.0
+ *******************************************************************************
+ */
+public class XxcsoSpDecisionHeadersSummuryVOImpl extends OAViewObjectImpl 
+{
+  /**
+   * 
+   * This is the default constructor (do not remove)
+   */
+  public XxcsoSpDecisionHeadersSummuryVOImpl()
+  {
+  }
+}
