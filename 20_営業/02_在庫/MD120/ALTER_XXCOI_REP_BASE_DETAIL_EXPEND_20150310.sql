3876123bcaf55b9611d8679e8d379e74788abbc3
Administrator <ITOEN.MASTER\Administrator>
2015-07-16 06:05:57 +0000

    02188
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C16249

---
+++ 20_営業/02_在庫/MD120/ALTER_XXCOI_REP_BASE_DETAIL_EXPEND_20150310.sql
@@ -0,0 +1,7 @@
+-- 払出明細票（拠点別・合計）帳票ワークテーブル項目追加スクリプト
+ALTER TABLE xxcoi.xxcoi_rep_base_detail_expend ADD (
+  inv_cl_char                    VARCHAR2(4)
+)
+/
+COMMENT ON COLUMN xxcoi.xxcoi_rep_base_detail_expend.inv_cl_char                   IS '在庫確定印字文字';
+/
