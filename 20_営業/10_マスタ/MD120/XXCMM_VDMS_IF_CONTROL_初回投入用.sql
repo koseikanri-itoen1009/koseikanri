c905ce90ad409791c091373f6d52cceda3680d37
Administrator <ITOEN.MASTER\Administrator>
2016-03-08 07:10:11 +0000

    02325
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C16941

---
+++ 20_営業/10_マスタ/MD120/XXCMM_VDMS_IF_CONTROL_初回投入用.sql
@@ -0,0 +1,26 @@
+INSERT INTO xxcmm.xxcmm_vdms_if_control (
+   control_id
+  ,vdms_interface_date
+  ,created_by
+  ,creation_date
+  ,last_updated_by
+  ,last_update_date
+  ,last_update_login
+  ,program_application_id
+  ,program_id
+  ,program_update_date
+)
+VALUES
+(
+   1
+  ,TO_DATE(:èââÒî‰ärì˙,'YYYY/MM/DD HH24:MI:SS')
+  ,-1
+  ,SYSDATE
+  ,-1
+  ,SYSDATE
+  ,-1
+  ,-1
+  ,-1
+  ,SYSDATE
+)
+/
