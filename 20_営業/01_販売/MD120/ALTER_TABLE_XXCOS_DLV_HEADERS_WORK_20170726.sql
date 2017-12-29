8932aecfb6a0b1fddb2baa840be73aebf4ed11f3
Administrator <ITOEN.MASTER\Administrator>
2017-12-29 04:35:48 +0000

    02587
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C18607

---
+++ 20_営業/01_販売/MD120/ALTER_TABLE_XXCOS_DLV_HEADERS_WORK_20170726.sql
@@ -0,0 +1,12 @@
+ALTER TABLE xxcos.xxcos_dlv_headers_work ADD (
+   visit_class1  VARCHAR2(2)
+  ,visit_class2  VARCHAR2(2)
+  ,visit_class3  VARCHAR2(2)
+  ,visit_class4  VARCHAR2(2)
+  ,visit_class5  VARCHAR2(2)
+);
+COMMENT ON COLUMN xxcos.xxcos_dlv_headers_work.visit_class1 IS '–K–â‹æ•ª1';
+COMMENT ON COLUMN xxcos.xxcos_dlv_headers_work.visit_class2 IS '–K–â‹æ•ª2';
+COMMENT ON COLUMN xxcos.xxcos_dlv_headers_work.visit_class3 IS '–K–â‹æ•ª3';
+COMMENT ON COLUMN xxcos.xxcos_dlv_headers_work.visit_class4 IS '–K–â‹æ•ª4';
+COMMENT ON COLUMN xxcos.xxcos_dlv_headers_work.visit_class5 IS '–K–â‹æ•ª5';
