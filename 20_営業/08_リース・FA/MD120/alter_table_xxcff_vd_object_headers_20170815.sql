03aeaddfa30b08cb63bd0889e3ae3f3a62d48ce3
Administrator <ITOEN.MASTER\Administrator>
2018-02-15 11:36:34 +0000

    02569
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C18821

---
+++ 20_営業/08_リース・FA/MD120/alter_table_xxcff_vd_object_headers_20170815.sql
@@ -0,0 +1,17 @@
+ALTER TABLE xxcff.xxcff_vd_object_headers ADD(
+    ifrs_life_in_months    NUMBER(3)
+   ,ifrs_cat_deprn_method  VARCHAR2(30)
+   ,real_estate_acq_tax    NUMBER(13)
+   ,borrowing_cost         NUMBER(13)
+   ,other_cost             NUMBER(13)
+   ,ifrs_asset_account     VARCHAR2(5)
+   ,correct_date           DATE
+);
+--
+COMMENT ON COLUMN xxcff.xxcff_vd_object_headers.ifrs_life_in_months       IS  'IFRS耐用年数';
+COMMENT ON COLUMN xxcff.xxcff_vd_object_headers.ifrs_cat_deprn_method     IS  'IFRS償却';
+COMMENT ON COLUMN xxcff.xxcff_vd_object_headers.real_estate_acq_tax       IS  '不動産取得税';
+COMMENT ON COLUMN xxcff.xxcff_vd_object_headers.borrowing_cost            IS  '借入コスト';
+COMMENT ON COLUMN xxcff.xxcff_vd_object_headers.other_cost                IS  'その他';
+COMMENT ON COLUMN xxcff.xxcff_vd_object_headers.ifrs_asset_account        IS  'IFRS資産科目';
+COMMENT ON COLUMN xxcff.xxcff_vd_object_headers.correct_date              IS  '修正年月日';
