3ec812264beb46766c9c1260602dc2c3f698619e
Administrator <ITOEN.MASTER\Administrator>
2019-02-12 03:56:18 +0000

    2723
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C19815

---
+++ 20_営業/10_マスタ/MD120/ALTER_TABLE_XXCMM_WK_CUST_BATCH_REGIST_20181228.sql
@@ -0,0 +1,6 @@
+ALTER TABLE XXCMM.XXCMM_WK_CUST_BATCH_REGIST  ADD (
+     latitude                                 VARCHAR2(20)    -- カテゴリー商品計上区分
+);
+--
+COMMENT ON COLUMN xxcmm.xxcmm_wk_cust_batch_regist.latitude  IS 'カテゴリー商品計上区分'
+/
