a762d8472071b66e9dd089be48b12d013410da80
Administrator <ITOEN.MASTER\Administrator>
2019-06-13 07:04:00 +0000

    02763
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C20040

---
+++ 20_営業/10_マスタ/MD120/ALTER_TABLE_XXCMM_WK_ITEM_BATCH_REGIST_20181206.sql
@@ -0,0 +1,4 @@
+ALTER TABLE xxcmm.xxcmm_wk_item_batch_regist ADD (
+  class_for_variable_tax VARCHAR2(100)
+);
+COMMENT ON COLUMN xxcmm.xxcmm_wk_item_batch_regist.class_for_variable_tax  IS 'åyå∏ê≈ó¶ópê≈éÌï ';
