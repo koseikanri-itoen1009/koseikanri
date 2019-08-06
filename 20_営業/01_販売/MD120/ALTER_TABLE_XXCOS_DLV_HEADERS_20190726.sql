2aa38fe917881bf08d77c79af4f706520e57a82c
Administrator <ITOEN.MASTER\Administrator>
2019-08-06 07:17:11 +0000

    02771
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C20301

---
+++ 20_営業/01_販売/MD120/ALTER_TABLE_XXCOS_DLV_HEADERS_20190726.sql
@@ -0,0 +1,5 @@
+ALTER TABLE xxcos.xxcos_dlv_headers ADD(
+  discount_tax_class              VARCHAR2(4)                       -- ’lˆøÅ‹æ•ª
+);
+--
+COMMENT ON COLUMN xxcos.xxcos_dlv_headers.discount_tax_class   IS '’lˆøÅ‹æ•ª';
