a183ea968e467ce9c3fa5cbbcd54cb69341bf742
Administrator <ITOEN.MASTER\Administrator>
2020-07-20 23:41:31 +0000

    02892
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C20984

---
+++ 20_営業/03_個別開発/MD120/ALTER_XXCOK_DEDU_SELL_TRNS_INFO.sql
@@ -0,0 +1,5 @@
+alter TABLE xxcok.XXCOK_DEDU_SELL_TRNS_INFO add  (
+  product_class                  VARCHAR2(40) 
+);
+COMMENT ON COLUMN xxcok.xxcok_dedu_sell_trns_info.product_class                        IS 'è§ïiãÊï™'
+/
