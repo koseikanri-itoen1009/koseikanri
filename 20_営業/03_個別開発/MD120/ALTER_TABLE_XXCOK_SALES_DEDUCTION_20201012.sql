176ffb2ba35872be3e9319d4cf6d25138725e12e
Administrator <ITOEN.MASTER\Administrator>
2020-12-17 06:08:07 +0000

    02938
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C21334

---
+++ 20_営業/03_個別開発/MD120/ALTER_TABLE_XXCOK_SALES_DEDUCTION_20201012.sql
@@ -0,0 +1,4 @@
+ALTER TABLE xxcok.xxcok_sales_deduction MODIFY ( compensation             NUMBER,
+                                                 margin                   NUMBER,
+                                                 sales_promotion_expenses NUMBER,
+                                                 margin_reduction         NUMBER);
