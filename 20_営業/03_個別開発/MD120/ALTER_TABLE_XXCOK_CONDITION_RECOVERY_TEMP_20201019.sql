aa0a3aebb7a3f0585207fde64f2b4114d4aaf909
Administrator <ITOEN.MASTER\Administrator>
2020-12-17 06:06:22 +0000

    02938
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C21332

---
+++ 20_営業/03_個別開発/MD120/ALTER_TABLE_XXCOK_CONDITION_RECOVERY_TEMP_20201019.sql
@@ -0,0 +1,16 @@
+ALTER TABLE xxcok.xxcok_condition_recovery_temp
+ADD(
+     compensation_en_3              NUMBER
+    ,wholesale_margin_en_3          NUMBER
+    ,just_condition_en_4            NUMBER
+    ,wholesale_adj_margin_en_4      NUMBER
+)
+/
+COMMENT ON COLUMN xxcok.xxcok_condition_recovery_temp.compensation_en_3                IS 'ï‚ìU(â~)'
+/
+COMMENT ON COLUMN xxcok.xxcok_condition_recovery_temp.wholesale_margin_en_3            IS 'ñ‚âÆÉ}Å[ÉWÉì(â~)'
+/
+COMMENT ON COLUMN xxcok.xxcok_condition_recovery_temp.just_condition_en_4              IS 'ç°âÒèåè(â~)'
+/
+COMMENT ON COLUMN xxcok.xxcok_condition_recovery_temp.wholesale_adj_margin_en_4        IS 'ñ‚âÆÉ}Å[ÉWÉìèCê≥(â~)'
+/
