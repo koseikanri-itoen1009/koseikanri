e3f7274edba757c5fd2871f1ccc1392ceee266db
Administrator <ITOEN.MASTER\Administrator>
2021-04-07 07:48:12 +0000

    02990
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C21572

---
+++ 20_営業/03_個別開発/MD120/ALTER_TABLE_XXCOK_CONDITION_TEMP_20210226.sql
@@ -0,0 +1,8 @@
+ALTER TABLE xxcok.xxcok_condition_temp 
+ADD (
+      accounting_customer_code          VARCHAR2(9)
+)
+/
+COMMENT ON COLUMN xxcok.xxcok_condition_temp.accounting_customer_code           IS 'åvè„å⁄ãq'
+/
+
