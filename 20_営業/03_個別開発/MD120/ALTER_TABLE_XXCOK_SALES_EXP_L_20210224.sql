b54409bf6350455b3fbd81e4bbf5261b4ee38154
Administrator <ITOEN.MASTER\Administrator>
2021-04-07 07:47:39 +0000

    02990
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C21571

---
+++ 20_営業/03_個別開発/MD120/ALTER_TABLE_XXCOK_SALES_EXP_L_20210224.sql
@@ -0,0 +1,10 @@
+ALTER TABLE xxcok.xxcok_sales_exp_l 
+ADD (
+      vessel_group                   VARCHAR2(4)
+     ,vessel_group_item_code         VARCHAR2(7)
+)
+/
+COMMENT ON COLUMN xxcok.xxcok_sales_exp_l.vessel_group           IS '—eŠíŒQ'
+/
+COMMENT ON COLUMN xxcok.xxcok_sales_exp_l.vessel_group_item_code IS '—eŠíŒQ•i–Ú'
+/
