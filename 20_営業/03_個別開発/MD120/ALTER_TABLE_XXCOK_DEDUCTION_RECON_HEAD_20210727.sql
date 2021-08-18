261b827cc3a7fbbb8224597a61129ee27271ee4f
Administrator <ITOEN.MASTER\Administrator>
2021-08-18 05:55:39 +0000

    03061
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C21884

---
+++ 20_営業/03_個別開発/MD120/ALTER_TABLE_XXCOK_DEDUCTION_RECON_HEAD_20210727.sql
@@ -0,0 +1,8 @@
+ALTER TABLE xxcok.xxcok_deduction_recon_head 
+ADD (
+      condition_no          VARCHAR2(1039)
+)
+/
+COMMENT ON COLUMN xxcok.xxcok_deduction_recon_head .condition_no           IS 'çTèúî‘çÜ'
+/
+
