536b25ea44f14a710932e20bed2cda6983becf1d
Administrator <ITOEN.MASTER\Administrator>
2021-10-07 10:57:09 +0000

    03066
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C21985

---
+++ 10_生産物流/MD120/ALTER_TABLE_XXWSH_SHIPPING_HEADERS_IF_20210824.sql
@@ -0,0 +1,4 @@
+ALTER TABLE xxwsh.xxwsh_shipping_headers_if ADD (
+  confirm_request_class  VARCHAR2(1)
+);
+COMMENT ON COLUMN xxwsh_shipping_headers_if.confirm_request_class IS '•¨—¬’S“–Šm”FˆË—Š‹æ•ª';
