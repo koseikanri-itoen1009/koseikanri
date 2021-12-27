868af44742f137e3a9164ef4324c67fd5e998b7e
Administrator <ITOEN.MASTER\Administrator>
2021-12-27 02:54:49 +0000

    03105
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C22118

---
+++ 20_営業/09_AR/MD120/ALTER_TABLE_XX03_RECEIVABLE_SLIPS_ERR_20211105.sql
@@ -0,0 +1,6 @@
+ALTER TABLE xx03.xx03_receivable_slips_err ADD (
+  payment_ele_data_yes  VARCHAR2(1),
+  payment_ele_data_no   VARCHAR2(1)
+);
+COMMENT ON COLUMN xx03.xx03_receivable_slips_err.payment_ele_data_yes IS '支払案内書電子データ受領あり';
+COMMENT ON COLUMN xx03.xx03_receivable_slips_err.payment_ele_data_no IS '支払案内書電子データ受領なし';
