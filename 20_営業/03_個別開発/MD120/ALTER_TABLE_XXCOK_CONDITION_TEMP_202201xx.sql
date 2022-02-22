ade0495a039d245797614b5953442ea4c6b9c0f1
Administrator <ITOEN.MASTER\Administrator>
2022-02-22 04:21:56 +0000

    03112
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C22206

---
+++ 20_営業/03_個別開発/MD120/ALTER_TABLE_XXCOK_CONDITION_TEMP_202201xx.sql
@@ -0,0 +1,7 @@
+ALTER TABLE xxcok.xxcok_condition_temp 
+ADD (
+      data_type_div                     VARCHAR2(1)
+)
+/
+COMMENT ON COLUMN xxcok.xxcok_condition_temp.data_type_div                      IS 'ÉfÅ[É^éÌóﬁãÊï™'
+/
