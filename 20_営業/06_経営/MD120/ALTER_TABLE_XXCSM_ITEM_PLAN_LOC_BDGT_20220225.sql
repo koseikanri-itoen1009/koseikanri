94a13670232801fc15e1422a1375482c73938416
Administrator <ITOEN.MASTER\Administrator>
2022-06-09 02:05:59 +0000

    03114
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C22512

---
+++ 20_営業/06_経営/MD120/ALTER_TABLE_XXCSM_ITEM_PLAN_LOC_BDGT_20220225.sql
@@ -0,0 +1,5 @@
+alter table xxcsm.xxcsm_item_plan_loc_bdgt ADD(
+  variable_consideration_kbn_a NUMBER(15)
+);
+COMMENT ON COLUMN xxcsm.xxcsm_item_plan_loc_bdgt.variable_consideration_kbn_a     IS '•Ï“®‘Î‰¿ŠzA';
+/
