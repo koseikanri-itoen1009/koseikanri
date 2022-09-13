91c19712458e4951df15a49715d1a2fd9b2ebd2a
Administrator <ITOEN.MASTER\Administrator>
2022-09-13 00:35:00 +0000

    03178
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C23338

---
+++ 20_営業/03_個別開発/MD120/ALTER_TABLE_XXCOK_DEDUCTION_RECON_HEAD_20220419.sql
@@ -0,0 +1,7 @@
+ALTER TABLE xxcok.xxcok_deduction_recon_head 
+ADD (
+     cancel_gl_date                 DATE
+)
+/
+COMMENT ON COLUMN xxcok.xxcok_deduction_recon_head.cancel_gl_date                      IS 'éÊè¡GLãLí†ì˙'
+/
