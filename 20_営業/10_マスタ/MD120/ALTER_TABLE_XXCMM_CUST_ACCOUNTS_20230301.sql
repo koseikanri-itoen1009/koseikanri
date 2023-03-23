574988741ffd7c42439d3adf20f4765f376ce920
Administrator <ITOEN.MASTER\Administrator>
2023-03-23 06:20:37 +0000

    03219
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C23651

---
+++ 20_営業/10_マスタ/MD120/ALTER_TABLE_XXCMM_CUST_ACCOUNTS_20230301.sql
@@ -0,0 +1,7 @@
+ALTER TABLE xxcmm.xxcmm_cust_accounts 
+ADD (
+      invoice_tax_div          VARCHAR2(1)
+)
+/
+COMMENT ON COLUMN xxcmm.xxcmm_cust_accounts.invoice_tax_div           IS '¿‹‘Á”ïÅÏã‚°ŒvZ•û®'
+/
