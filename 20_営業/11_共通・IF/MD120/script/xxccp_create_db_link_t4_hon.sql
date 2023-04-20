a3fab39afdb942c59d99669a03376ef134e7e874
Administrator <ITOEN.MASTER\Administrator>
2023-04-20 00:33:57 +0000

    03189
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C23683

--- 20_営業/11_共通・IF/MD120/script/xxccp_create_db_link_t4_hon.sql
+++ 20_営業/11_共通・IF/MD120/script/xxccp_create_db_link_t4_hon.sql
@@ -3,22 +3,23 @@
  *
  * DATABASE LINK Name : T4_HON
  * Description        : T4→本番環境のデータベースリンク
- * Version            : 1.0
+ * Version            : 1.1
  * 
  * Change Record
  * ------------- ----- ------------ -------------------------------------
  *  Date          Ver.  Editor       Description
  * ------------- ----- ------------ -------------------------------------
  *  2015/10/29    1.0   S.Niki       新規作成
+ *  2022/11/01    1.1   H.Iitsuka    Iaas Rift 後の環境に修正
  ************************************************************************/
 CREATE DATABASE LINK t4_hon CONNECT TO APPS IDENTIFIED BY APPS
 USING '(DESCRIPTION=
          (ADDRESS_LIST=
-           (ADDRESS=(PROTOCOL=tcp)(HOST=aebsdb31.itoen.master)(PORT=1521))
-           (ADDRESS=(PROTOCOL=tcp)(HOST=aebsdb21.itoen.master)(PORT=1521))
-           (ADDRESS=(PROTOCOL=tcp)(HOST=aebsdb11.itoen.master)(PORT=1521))
+           (ADDRESS=(PROTOCOL=tcp)(HOST=bebsdb31.itoen.master)(PORT=1521))
+           (ADDRESS=(PROTOCOL=tcp)(HOST=bebsdb21.itoen.master)(PORT=1521))
+           (ADDRESS=(PROTOCOL=tcp)(HOST=bebsdb11.itoen.master)(PORT=1521))
          )
          (CONNECT_DATA=
-           (SERVICE_NAME=AEBSITO.itoen.master))
+           (SERVICE_NAME=BEBSITO.itoen.master))
       )'
 ;
