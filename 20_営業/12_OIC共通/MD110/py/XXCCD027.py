e1d1cca3e4966d0caf9aa00b5d7a13986a468fb8
Administrator <ITOEN.MASTER\Administrator>
2023-11-21 00:57:15 +0000

    03297
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C25118

--- 20_営業/12_OIC共通/MD110/py/XXCCD027.py
+++ 20_営業/12_OIC共通/MD110/py/XXCCD027.py
@@ -8,13 +8,15 @@
           作成者  ：   SCSK   吉岡           2023/02/15 Draft1A
           更新履歴：   SCSK   吉岡           2023/02/15 Draft1A  初版
                                              2023/02/28 Issue1.0 Issue化
-
+                       SCSK   細沼           2023/11/08 Issue1.1 E_本稼動_19588
      [戻り値]
         0 : 正常
         8 : 異常
 
      [パラメータ]
         取得日                               string
+        取得時刻From                         string
+        取得時刻To                           string
 
      [使用方法]
         /uspg/jp1/zb/py/T4/XXCCD027.py
@@ -30,20 +32,36 @@ import datetime
 
 from com.Xxccdcomn import Xxccdcomn
 from com.PyComnException import PyComnException
+from requests.exceptions import ConnectTimeout
+
 
 def execApi(com, execPayload, exeName, apiUrl):
 
-    ## RESTAPI実行
-    apiResponse = requests.request("GET"
-        , com.getEnvValue("OIC_HOST") + apiUrl
-        , headers=com.headers
-        , params=execPayload)
+    try:
+        ## RESTAPI実行
+        apiResponse = requests.request("GET"
+            , com.getEnvValue("OIC_HOST") + apiUrl
+            , headers=com.headers
+            , params=execPayload
+            , timeout=(float(com.getEnvConstValue("REST_CONN_TIMEOUT_SEC")), None))
 
-    ## RESTAPIエラー判定
-    com.oicErrChk(apiResponse, exeName)
+        ## RESTAPIエラー判定
+        com.oicErrChk(apiResponse, exeName)
+
+        return apiResponse
+
+    ## 例外判定
+    except ConnectTimeout as eCT:
+        com.endCd = com.getEnvConstValue("JP1_ERR_CD")
+        
+        callName = ""
+        if exeName:
+            callName = exeName[0]
+            
+        raise PyComnException("CCDE0010", [callName, str(eCT)])
 
     return apiResponse
-
+                
 ### Main処理 ###
 def main():
     ## 共通処理インスタンス生成
@@ -53,17 +71,28 @@ def main():
     try:
         ## パラメータチェック、コマンドライン引数型変換
         prmGetDate = com.paramChkAndConv("取得日", args[1], False, "str", 0)
+        prmGetTimeF = com.paramChkAndConv("取得時刻From", args[2], False, "str", 0)
+        prmGetTimeT = com.paramChkAndConv("取得時刻To", args[3], False, "str", 0)
 
         ##### OIC実行ログ定期ダウンロードAPI実行 #####
         ## 入力パラメータ生成
         nowDate = datetime.datetime.now()
-        prmDate = (nowDate + datetime.timedelta(days=-1)).strftime("%Y-%m-%d")
+        prmDate = (nowDate + datetime.timedelta(days=-2)).strftime("%Y-%m-%d")
+        prmTimeF = "00:00:00"
+        prmTimeT = "23:59:59"
+
         #入力パラメータ.取得日が設定されている場合は入力パラメータ.取得日を設定
         if prmGetDate:
             prmDate = prmGetDate
 
+        #入力パラメータ.取得時刻From・Toが設定されている場合はそれぞれをパラメータに設定
+        if prmGetTimeF:
+            prmTimeF = prmGetTimeF
+        if prmGetTimeT:
+            prmTimeT = prmGetTimeT
+
         execPayload = {
-            "q" : "{startdate : '" + prmDate + " 00:00:00' , enddate : '" + prmDate + " 23:59:59'}"
+            "q" : "{startdate : '" + prmDate + " "+ prmTimeF + "' , enddate : '" + prmDate + " " + prmTimeT + "'}"
         }
 
         ## RESTAPI実行、エラー判定
@@ -71,7 +100,9 @@ def main():
             , "/ic/api/integration/v1/monitoring/logs/icsflowlog/")
 
         ## 実行結果をzipファイル保存
-        zipFileName = com.getEnvConstValue("ICSFLOWLOG_ZIP_FILENAME") + "_" + nowDate.strftime("%Y%m%d") + ".zip"
+        zipFileName = com.getEnvConstValue("ICSFLOWLOG_ZIP_FILENAME") + "_" + prmDate.replace("-", "") + \
+                        prmTimeF.replace(":", "") + ".zip"
+        
         with open(com.getEnvValue("ICSFLOWLOG_ZIP_FILEPATH") + zipFileName, "wb") as f:
             f.write(apiResponse.content)
 
