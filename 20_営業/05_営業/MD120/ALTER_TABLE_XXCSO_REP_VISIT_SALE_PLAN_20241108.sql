e64a4c1a3a0b32f4b1095e743e6a81482f1a6169
Administrator <ITOEN.MASTER\Administrator>
2024-11-28 01:01:35 +0000

    03397
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C26060

---
+++ 20_営業/05_営業/MD120/ALTER_TABLE_XXCSO_REP_VISIT_SALE_PLAN_20241108.sql
@@ -0,0 +1,106 @@
+-- –K–â”„ãŒv‰æŠÇ—•\’ •[ƒ[ƒNƒe[ƒuƒ‹€–Ú’Ç‰Á
+ALTER TABLE xxcso.xxcso_rep_visit_sale_plan ADD(
+    num_of_visits           NUMBER(15),
+    visits_rank             VARCHAR2(1),
+    rslt_vis_num_total      NUMBER(12),
+    num_of_visits_day_1     NUMBER(1),
+    num_of_visits_day_2     NUMBER(1),
+    num_of_visits_day_3     NUMBER(1),
+    num_of_visits_day_4     NUMBER(1),
+    num_of_visits_day_5     NUMBER(1),
+    num_of_visits_day_6     NUMBER(1),
+    num_of_visits_day_7     NUMBER(1),
+    num_of_visits_day_8     NUMBER(1),
+    num_of_visits_day_9     NUMBER(1),
+    num_of_visits_day_10    NUMBER(1),
+    num_of_visits_day_11    NUMBER(1),
+    num_of_visits_day_12    NUMBER(1),
+    num_of_visits_day_13    NUMBER(1),
+    num_of_visits_day_14    NUMBER(1),
+    num_of_visits_day_15    NUMBER(1),
+    num_of_visits_day_16    NUMBER(1),
+    num_of_visits_day_17    NUMBER(1),
+    num_of_visits_day_18    NUMBER(1),
+    num_of_visits_day_19    NUMBER(1),
+    num_of_visits_day_20    NUMBER(1),
+    num_of_visits_day_21    NUMBER(1),
+    num_of_visits_day_22    NUMBER(1),
+    num_of_visits_day_23    NUMBER(1),
+    num_of_visits_day_24    NUMBER(1),
+    num_of_visits_day_25    NUMBER(1),
+    num_of_visits_day_26    NUMBER(1),
+    num_of_visits_day_27    NUMBER(1),
+    num_of_visits_day_28    NUMBER(1),
+    num_of_visits_day_29    NUMBER(1),
+    num_of_visits_day_30    NUMBER(1),
+    num_of_visits_day_31    NUMBER(1)
+)
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits           IS '–K–âŠî€‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.visits_rank             IS '–K–âƒ‰ƒ“ƒN'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.rslt_vis_num_total      IS '–K–âÀÑ‡Œv'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_1     IS '‚P“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_2     IS '‚Q“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_3     IS '‚R“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_4     IS '‚S“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_5     IS '‚T“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_6     IS '‚U“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_7     IS '‚V“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_8     IS '‚W“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_9     IS '‚X“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_10    IS '‚P‚O“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_11    IS '‚P‚P“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_12    IS '‚P‚Q“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_13    IS '‚P‚R“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_14    IS '‚P‚S“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_15    IS '‚P‚T“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_16    IS '‚P‚U“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_17    IS '‚P‚V“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_18    IS '‚P‚W“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_19    IS '‚P‚X“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_20    IS '‚Q‚O“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_21    IS '‚Q‚P“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_22    IS '‚Q‚Q“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_23    IS '‚Q‚R“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_24    IS '‚Q‚S“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_25    IS '‚Q‚T“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_26    IS '‚Q‚U“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_27    IS '‚Q‚V“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_28    IS '‚Q‚W“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_29    IS '‚Q‚X“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_30    IS '‚R‚O“ú|Œv‰æ–K–â‰ñ”'
+/
+COMMENT ON COLUMN xxcso.xxcso_rep_visit_sale_plan.num_of_visits_day_31    IS '‚R‚P“ú|Œv‰æ–K–â‰ñ”'
+/
---
+++ 20_営業/05_営業/MD120/DROP_XXCSO_TMP_REP_VS_PLAN.sql
@@ -0,0 +1,1 @@
+DROP TABLE xxcso.xxcso_tmp_rep_vs_plan;
