453213d9614307d5289b8b7cca9cbb2d2c1f991a
Administrator <ITOEN.MASTER\Administrator>
2025-03-04 05:56:02 +0000

    3438
    git-tfs-id: [http://acomsv01:8080/tfs/DefaultCollection]$/U5000PRG;C26248

---
+++ 20_営業/12_OIC共通/MD120/insert_table_xxccd_profile_option_values_20241211.sql
@@ -0,0 +1,4 @@
+-- プロファイル値テーブル
+--
+Insert into XXCCD_PROFILE_OPTION_VALUES values (34,'10001','0',null,SYS_CONTEXT('USERENV','SESSION_USER'),systimestamp at time zone('UTC'),SYS_CONTEXT('USERENV','SESSION_USER'),systimestamp at time zone('UTC'));
+Insert into XXCCD_PROFILE_OPTION_VALUES values (35,'10001','0',TO_CHAR( systimestamp at time zone('JAPAN'), 'YYYY-MM-DD HH24:MI:SS' ),SYS_CONTEXT('USERENV','SESSION_USER'),systimestamp at time zone('UTC'),SYS_CONTEXT('USERENV','SESSION_USER'),systimestamp at time zone('UTC'));
